66d0753c7a15f5a7bb588ddb6359a1e90f3069e1
Kishy-nivas <kishynivas10@gmail.com>
2017-12-15 08:15:50 +0530

    Used reset

--- db/migrate/20171208115257_create_votes.rb
+++ db/migrate/20171208115257_create_votes.rb
@@ -1,9 +1,8 @@
--class CreateVotes < ActiveRecord::Migration[5.1]
--  def change
--    create_table :votes do |t|
--      t.integer :story_id
--
--      t.timestamps
--    end
--  end
--end
+class CreateVotes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :votes do |t|
+      t.integer :story_id 
+      t.timestamps
+    end
+  end
+end
